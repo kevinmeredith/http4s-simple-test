f9281f8593f107c56d31cb78961873f461ddd82c
Kevin Meredith <kevin.m.meredith@gmail.com>
2016-09-07 22:09:42 -0400

    remove helloWorldHttpService

--- src/main/scala/net/Main.scala
+++ src/main/scala/net/Main.scala
@@ -3,7 +3,6 @@ package net
 import net.repository.PersonRepositoryImpl
 import org.http4s.server.{Server, ServerApp}
 import org.http4s.server.blaze.BlazeBuilder
-import net.web.HelloWorldHttpService.helloWorldService
 import net.web.PersonHttpService.personService
 
 import scalaz.concurrent.Task
@@ -13,7 +12,6 @@ object Main extends ServerApp {
   override def server(args: List[String]): Task[Server] = {
     BlazeBuilder
       .bindHttp(8080, "localhost")
-      .mountService(helloWorldService, "/api")
       .mountService(personService(PersonRepositoryImpl), "/api")
       .start
   }
